1b48d5daa9d4d4d756fc58b5cc747fb61e6e9551
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2024-07-20 06:26:19 +0300

    Main menu reskin & input system fix

--- Assets/Scripts/VR/GameVersion.cs
+++ Assets/Scripts/VR/GameVersion.cs
@@ -1,6 +1,6 @@
 public static class GameVersion
 {
     public const string gameName = "Faz-Anim";
-    public const string gameVersion = "1.60";
+    public const string gameVersion = "1.60-ALPHA";
     public const string isVR = "false";
 }
